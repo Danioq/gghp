8597ca266b2a40e4089f6a5476bb9ca3d396f034
Daniel Łobacz <229363@student.pwr.edu.pl>
2020-09-20 19:00:40 +0200

    feat add mutex to sensor list

--- sensors/sensors.go
+++ sensors/sensors.go
@@ -1,17 +1,24 @@
 package sensors
 
+import "sync"
+
 type Sensor struct {
 	Value float64 `json:"value"`
 	Name  string  `json:"name"`
 }
 
+var mutex sync.Mutex
 var sensorsList = map[string]Sensor{}
 
 func AddSensor(name string, value float64) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	sensorsList[name] = Sensor{Name: name, Value: value}
 }
 
 func GetSensor(name string) (*Sensor, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
 	sensor, ok := sensorsList[name]
 	if ok {
 		return &sensor, nil
